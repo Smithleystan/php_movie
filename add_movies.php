3218fe4f9e9253c0469c7ce258b68607dd8a5092
stan <stan.smithley@gmail.com>
2023-01-26 14:06:53 +0100

    suivant

--- add_movies.php
+++ add_movies.php
@@ -1,4 +1,31 @@
 <?php
+if (!empty($_POST)) {
+    $title = trim(strip_tags($_POST["title"]));
+    $genres = trim(strip_tags($_POST["genres"]));
+    $description = trim(strip_tags($_POST["description"]));
+    $releaseDate = trim(strip_tags($_POST["releaseDate"]));
+    $duration = trim(strip_tags($_POST["duration"]));
+    $video = trim(strip_tags($_POST["video"]));
+
+    // var_dump($releaseDate);
+
+    $sdn = "mysql:host=localhost;dbase=stan";
+    $db = new PDO($sdn, "root", "");
+
+    $query = $db->prepare("INSERT INTO stan.movies (title, genres, description, release_Date, duration, video) VALUES (:title, :genres, :description, :releaseDate, :duration, :video)");
+
+    $query->bindParam("title", $title);
+    $query->bindParam("genres", $genres);
+    $query->bindParam("description", $description);
+    $query->bindParam("releaseDate", $releaseDate, PDO::PARAM_STR);
+    $query->bindParam("duration", $duration, PDO::PARAM_INT);
+    $query->bindParam("video", $video);
+
+    if ($query->execute()) {
+        header("Location: index.php");
+    }
+}
+
 include("./templates/header.php");
 ?>
 
@@ -35,6 +62,6 @@ include("./templates/header.php");
     </div>
     <br>
 
-
+    <input type="submit" value="valider">
 
 </form>
